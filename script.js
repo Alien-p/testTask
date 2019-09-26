92e2fe8d00bf1f70fdad9ad60a2f25b02037834f
Ildar Gumerov <Ildar.Gumerov@icl-services.com>
2019-09-26 17:32:04 +0300

    Add logic to ready button

--- script.js
+++ script.js
@@ -1,37 +1,87 @@
-let activeBtns = [];
+let activeBtnsText = [];
+
 let readyBtn =  $('#ready');
+let rightAnswers = ["x+5=11", "5+x=11"];
+rightAnswers.sort();
 
 $('button#answer').click( function() {
     $(this).toggleClass("buttonClicked");
-
-    checkBtnInList(this.textContent);
+    
+    checkBtnInList(this);
 });
 
 // Activate-disactivate answer button
-function checkBtnInList (btn) {
-    let btnIndex = activeBtns.indexOf(btn);
+function checkBtnInList (button) {
+    let btnText = button.textContent;
+    let btnIndex = activeBtnsText.indexOf(btnText);
     
-    if(btnIndex != -1 && activeBtns.length == 1) {
-        activeBtns.splice(0, 1);
+    if(btnIndex != -1 && activeBtnsText.length == 1) {
+        activeBtnsText.splice(0, 1);
         readyBtn.toggleClass("buttonReadyActive");
         readyBtn.attr("disabled", true);
-    } else if(btnIndex == -1 && activeBtns.length == 0) {
-        activeBtns.push(btn);
-        readyBtn.toggleClass("buttonReadyActive");
+    } else if(btnIndex == -1 && activeBtnsText.length == 0) {
+        activeBtnsText.push(btnText);
         readyBtn.attr("disabled", false);
-    } else if( btnIndex == -1 && activeBtns.length >= 1) {
-        activeBtns.push(btn);
+        readyBtn.toggleClass("buttonReadyActive");
+    } else if( btnIndex == -1 && activeBtnsText.length >= 1) {
+        activeBtnsText.push(btnText);
     } else {
-        activeBtns.splice(btnIndex, 1);
+        activeBtnsText.splice(btnIndex, 1);
     }
+
+    activeBtnsText.sort();
 }
 
 /**
  * Checking answers
  */
 
+ function markAnswers (readyBtn) {
+    let activeBtns = [];
+
+    if( JSON.stringify(activeBtnsText) == JSON.stringify(rightAnswers) ) {
+        $(readyBtn).addClass("buttonReadyCorrect"); 
+    } else {
+        activeBtns = Array.from( $('.buttonClicked') );
+        
+        activeBtns.forEach(btn => {
+            let btnIndex = rightAnswers.indexOf(btn.textContent);
+            if(btnIndex == -1) {
+                btn.classList.remove("buttonClicked");
+                btn.classList.add("wrongBtn");
+                readyBtn.classList.add("wrongBtn"); 
+
+                document.getElementById('hint').textContent = "Вычисли x"
+                document.getElementById('hint').style.visibility = "visible";
+            }
+        });
+    }
+}
+
+function refreshBtns() {
+    setTimeout(() => {
+        $('#hint').removeClass();
+        $('#answer').removeClass();
+        $('#ready').removeClass();
+    }, 1000);
+}
+
 $('#ready').click( function() {
-    
+
+    markAnswers(this);
+
+    refreshBtns();
+
+    //  {
+    //     $(this).addClass("buttonReadyWrong");
+
+    //  } 
+
+    // setTimeout(() => {
+    //     $(this).removeClass("buttonReadyWrong");
+
+    // }, 1000);
+
 })
 
 
